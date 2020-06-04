59db4a1de7dcb242984f6c335338e0ad318f2512
Mega-Gorilla <hahahadesu@gmail.com>
2020-06-05 00:41:04 +0900

    pyaudio インストール用ファイル追加

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ import speech_recognition
 r = speech_recognition.Recognizer()
 with speech_recognition.AudioFile('hoge.wav') as src:
     audio = r.record(src)
-print(r.recognize_google(audio, key='Your API Key', language='ja-JP'))
+print(r.recognize_google(audio, language='ja-JP'))
